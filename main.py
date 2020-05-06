5f5fe28d57f134d9e2e7764766bb86e29cced9a2
mvaario <mika.vaario@gmail.com>
2020-05-06 21:14:15 +0300

    - Update

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ class game:
         pg.mixer.init()
         pg.display.set_caption(TITLE)
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.key.set_repeat(500,100)
         self.clock = pg.time.Clock()
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
         self.load_data()
 
     def load_data(self):
@@ -28,44 +29,42 @@ class game:
 
     def new(self):
         # Start a new game
-        self.all_sprites = pg.sprite.Group()
-        self.walls = pg.sprite.Group()
-        for row, tiles in enumerate(self.map.data):
+        for row, tiles in enumerate(g.map.data):
             for col, tile in enumerate(tiles):
                 if tile == '1':
-                    Wall(self, col, row)
+                    Wall(g, col, row)
                 if tile == 'p':
-                    self.player = player(self, col, row)
-        self.camera = Camera(self.map.width, self.map.height)
+                    g.player = player(g, col, row)
+        g.camera = Camera(g.map.width, g.map.height)
 
 
     def run(self):
         # Game loop
-        self.clock.tick(FPS)
-        self.playing = True
-        while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
-            self.events()
-            self.update()
-            self.draw()
+        g.clock.tick(FPS)
+        g.playing = True
+        while g.playing:
+            g.dt = g.clock.tick(FPS) / 1000
+            g.events()
+            g.update()
+            g.draw()
 
     def update(self):
         # Game loop - update
-        self.all_sprites.update()
-        self.camera.update(self.player)
+        g.all_sprites.update()
+        g.camera.update(g.player)
 
     def draw_grid(self):
         for x in range(0, WIDTH, Tilesize):
-            pg.draw.line(self.screen, LightGray, (x, 0), (x, HEIGHT))
+            pg.draw.line(g.screen, LightGray, (x, 0), (x, HEIGHT))
         for y in range(0, HEIGHT, Tilesize):
-            pg.draw.line(self.screen, LightGray, (0, y), (WIDTH, y))
+            pg.draw.line(g.screen, LightGray, (0, y), (WIDTH, y))
 
     def draw(self):
         # Game loop draw
-        self.screen.fill(BGColor)
-        self.draw_grid()
-        for sprite in self.all_sprites:
-            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        g.screen.fill(BGColor)
+        g.draw_grid()
+        for sprite in g.all_sprites:
+            g.screen.blit(sprite.image, g.camera.apply(sprite))
         pg.display.flip()
 
     def events(self):
@@ -73,10 +72,10 @@ class game:
         for event in pg.event.get():
             # closing the window
             if event.type == pg.QUIT:
-                self.quit()
+                g.quit()
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_q:
-                    self.quit()
+                    g.quit()
 
 
     def show_start_screen(self):
@@ -99,6 +98,6 @@ g.show_start_screen()
 while True:
     g.new()
     g.run()
-    g.show_go_screen()
+
 
 pg.quit()
